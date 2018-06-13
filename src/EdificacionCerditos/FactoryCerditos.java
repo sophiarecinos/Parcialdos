c4d743feaedbbc7c6fcf9a0b34ee80a2a9c651e0
Sophi <Sophi@Tic-FR-Test-PC>
2018-06-12 21:59:43 -1200

    Modificar paquetes, agregar clases

---
+++ src/EdificacionCerditos/FactoryCerditos.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EdificacionCerditos;
+
+import Razas.*;
+/**
+ *
+ * @author Sophi
+ */
+public class FactoryCerditos implements RazasFactory{
+    
+    @Override
+    public Esqueletos getEsqueletos(int option){
+        return null;
+    }
+    
+    @Override
+    public Princesas getPrincesas(int option){
+        return null;
+    }
+    
+    @Override
+    public Cerditos getCerditos(int option){
+        //dar opciones para crear edificaciones con switch y validando recursos
+        return null;
+    }
+
+}
