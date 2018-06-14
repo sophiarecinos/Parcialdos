af946f075f54227e4441e4dca6329c58e1ae995f
Sophi <Sophi@Tic-FR-Test-PC>
2018-06-14 07:42:44 -1200

    Modificar menu

--- src/Main/Menu.java
+++ src/Main/Menu.java
@@ -14,7 +14,7 @@ public class Menu {
     Player p2 = Player.getInstance();
     Scanner in = new Scanner(System.in);
     public void MenuUno(){
-        int opcion = 0;
+        //int opcion = 0;
         
         //System.out.println("--------------------------------------");
         System.out.println("------ SOPHIA RECINOS WORLD -------");
@@ -29,6 +29,7 @@ public class Menu {
         System.out.println("--------------------------------------");
         raza=in.nextInt();
         p1.setRaza(raza);
+        p1.setNum(1);
         //p1.guardarCentroMando(raza);
         //System.out.println(p1.getRaza());
         System.out.println("-----------Jugador 2--------------------");
@@ -39,13 +40,40 @@ public class Menu {
         System.out.println("--------------------------------------");
         raza=in.nextInt();
         p2.setRaza(raza);
+        p2.setNum(2);
         //p2.guardarCentroMando(raza);   
        
         
     }
     public void MenuDos(){
-        
-        
+        int opcion = 0;
+        int cont = 0;
+        while(opcion!=4){
+            if((cont%2)==0)
+                System.out.println("----------Jugador 1---------------------");
+            else
+                System.out.println("----------Jugador 2---------------------");
+            
+            //System.out.println("----------Jugador A---------------------");
+            System.out.println("¿Que desea hacer?");
+            System.out.println("1. Modificar/ver mis edificaciones");
+            System.out.println("2. Atacar contrincante");
+            System.out.println("3. Defender mi territorio");
+            System.out.println("4. Acabar turno");
+            opcion=in.nextInt();
+            
+            switch(opcion){
+                case 1:
+                    
+                    break;
+                case 2:
+                case 3:
+                case 4: 
+                    opcion=0;
+            }
+            cont++;
+            
+        }
         
        
         
